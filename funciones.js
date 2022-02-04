3abce08f15b8aa260ace70fe558e18cbb7a1e4c5
Alfonso Moran <alfonso.amt@outlook.com>
2022-02-04 12:19:14 -0600

    Introducción a JavaScript

---
+++ funciones.js
@@ -0,0 +1,13 @@
+// Declarativas
+
+function miFunction(){
+    return 3;
+}
+
+// Expresión
+
+var miFuncion = function(a, b){
+    return a +  b;
+}
+
+miFuncion();
---
+++ variables.js
@@ -0,0 +1,18 @@
+var nombre = "Oscar";
+
+// declarar
+var edad;
+
+edad
+// inicializar
+edad = 30;
+edad
+
+var elemento = ["Computadora", "Celular"];
+
+var persona = {
+    nombre: "Diego",
+    edad: 30
+}
+
+persona;
