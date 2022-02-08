5474715943b6928294fda29678aa188681bded8f
Alfonso Moran <alfonso.amt@outlook.com>
2022-02-08 11:44:34 -0600

    Arrays

---
+++ CursoBasicoDeJavaScript/Arrays/arrays.js
@@ -0,0 +1,20 @@
+var frutas = ["Manzna", "Plátano", "Cereza", "Fresa"];
+
+console.log(frutas);
+
+console.log(frutas.length); // 4
+
+console.log(frutas[0]); // Manzana
+console.log(frutas[2]); // Cereza
+
+var masFrutas = frutas.push("Uvas");
+var ultimo = frutas.pop("Uvas");
+frutas
+
+var nuevaLonitud = frutas.unshift("Uvas");
+
+var borrarFrutas = fruta.shift("Uvas");
+
+var posicion = frutas.indexOf("Cereza");
+
+// No es necesario crear las variables y no es necesario pasar lo parametros a pop y shift
