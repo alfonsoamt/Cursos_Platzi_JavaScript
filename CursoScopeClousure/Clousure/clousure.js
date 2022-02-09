14ed3415d0fef6516a7e2b29565d9fc25e2b5755
Alfonso Moran <alfonso.amt@outlook.com>
2022-02-09 17:54:59 -0600

    Clousures

---
+++ CursoScopeClousure/Clousure/clousure.js
@@ -0,0 +1,8 @@
+const moneyBox = (coins) => {
+    var saveCoins = 0;
+    saveCoins += coins;
+    console.log(`MoneyBox: $${saveCoins}`);
+};
+
+moneyBox(5);
+moneyBox(10);
---
+++ CursoScopeClousure/Clousure/lexical.js
@@ -0,0 +1,16 @@
+const builtCount = (i) => {
+    let count = i;
+    const displayCount = () => {
+        console.log(count++);
+    };
+    return displayCount;
+};
+
+const myCount = builtCount(1);
+myCount();
+myCount();
+myCount();
+
+const myOtherCount = builtCount(10);
+myOtherCount();
+myOtherCount();
