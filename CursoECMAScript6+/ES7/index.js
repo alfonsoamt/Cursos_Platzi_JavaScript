2d4f4c18a143cef8ab7dbd7fc6dfce557a28a6d4
Alfonso Moran <alfonso.amt@outlook.com>
2022-02-10 13:28:38 -0600

    ES7

---
+++ CursoECMAScript6+/ES7/index.js
@@ -0,0 +1,13 @@
+let  numbers = [1, 2, 3, 7, 8];
+
+if  (numbers.includes(9)){
+    console.log('Sí se encuenta el valor 7');
+} else {
+    console.log('No se encuentra el valor');
+}
+
+let base = 4;
+let exponents = 3;
+let result = base ** exponents;
+
+console.log(result);
