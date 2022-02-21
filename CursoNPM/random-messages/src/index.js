f1118ed4c57d95d6c32db32b08761aeab2c2023d
Alfonso Moran <alfonso.amt@outlook.com>
2022-02-21 16:22:28 -0600

    CursoNPM

---
+++ CursoNPM/random-messages/src/index.js
@@ -0,0 +1,16 @@
+const messages = [
+    "oscar",
+    "diego",
+    "laura",
+    "ana",
+    "nicole",
+    "carolina",
+    "paulina"
+]
+
+const randomMsg = () => {
+    const message = messages[Math.floor(Math.random() * messages.length)];
+    console.log(message);
+};
+
+module.exports = { randomMsg };
