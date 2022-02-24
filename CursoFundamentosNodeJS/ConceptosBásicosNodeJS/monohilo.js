7c32d4fa6ace7c89bd5e8fb3880516169e93fec1
Alfonso Moran <alfonso.amt@outlook.com>
2022-02-23 18:21:55 -0600

    BasicNodeJs

---
+++ CursoFundamentosNodeJS/ConceptosBásicosNodeJS/monohilo.js
@@ -0,0 +1,12 @@
+console.log('Hola Mundo');
+
+setInterval(function() {
+    console.log("sigo Activo");}, 1000);
+
+let i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+    }, 1000);
+
+console.log("segunda instrucción");
