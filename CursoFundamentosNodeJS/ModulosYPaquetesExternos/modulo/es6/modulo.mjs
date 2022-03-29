ae47dc4348b57cf55afc9daed50c7463b8dfc6fb
Alfonso Moran <alfonso.amt@outlook.com>
2022-03-28 18:06:57 -0600

    Modulos y Paquetes Externos NodeJS

---
+++ CursoFundamentosNodeJS/ModulosYPaquetesExternos/modulo/es6/modulo.mjs
@@ -0,0 +1,6 @@
+function saludar (){
+    console.log('Hola mundo!!');
+}
+
+export default {saludar,
+                prop1: 'Soy un modulo experimental',};
---
+++ CursoFundamentosNodeJS/ModulosYPaquetesExternos/modulo/index.js
@@ -0,0 +1,7 @@
+// Traer nuestro modulo
+const modulo = require('./modulo');
+
+
+// Ejecutar una función del modulo
+console.log(modulo.prop1)
+modulo.saludar();
